8141e9ecf7287abc441a229f30a3606c4598fdde
Elmi <ElmiAli05@outlook.com>
2026-07-16 19:31:28 +0100

    improved

--- test2.py
+++ test2.py
@@ -1,9 +1,14 @@
 import random
 
-target = random.randint(1,1000)
-
+target = 0
+level = input("easy or hard: ")
+if level == "easy":
+    target = random.randint(1,50)
+elif level == "hard":
+    target = random.randint(1,1000)
+hashmap = {"easy": "1 - 50" , "hard" : "1 - 1000"}
 while True:
-    choice = int(input("enter a number: "))
+    choice = int(input(f"enter a number {hashmap[level]} : "))
     if choice == target:
         print("well done you guessed right")
         break
