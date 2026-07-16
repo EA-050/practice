c376b87cb2bde966aa94bc5e37b0f426a7724486
Elmi <ElmiAli05@outlook.com>
2026-07-16 19:13:33 +0100

    add second practice file

--- test2.py
+++ test2.py
@@ -1,1 +1,14 @@
-print("hello world")
+import random
+
+target = random.randint(1,1000)
+
+while True:
+    choice = int(input("enter a number: "))
+    if choice == target:
+        print("well done you guessed right")
+        break
+    elif choice > target:
+        print("you guess is too high try again")
+    else:
+        print("you guessed too low try again")
+
